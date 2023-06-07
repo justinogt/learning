cbd9cec844e1811cc1910b3f8f14f3d793c3fa86
Jorge Justino <jorgejustinogt@gmail.com>
2023-06-06 21:02:14 -0300

    enum

--- microsoft-rust-learning-path/src/main.rs
+++ microsoft-rust-learning-path/src/main.rs
@@ -1,7 +1,34 @@
-fn main() {
-  // Declare a tuple of three elements
-  let (letter, number, bool_value) = ('E', 5i32, true);
+// Define a tuple struct
+#[derive(Debug)]
+struct KeyPress(String, char);
 
-  // Use tuple indexing and show the values of the elements in the tuple
-  println!("Is '{}' the {}th letter of the alphabet? {}", letter, number, bool_value);
+// Define a classic struct
+#[derive(Debug)]
+struct MouseClick { x: i64, y: i64 }
+
+// Define the WebEvent enum variants to use the data from the structs
+// and a boolean type for the page Load variant
+#[derive(Debug)]
+enum WebEvent { WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress) }
+
+fn main() {
+    // Instantiate a MouseClick struct and bind the coordinate values
+    let click = MouseClick { x: 100, y: 250 };
+    println!("Mouse click location: {}, {}", click.x, click.y);
+        
+    // Instantiate a KeyPress tuple and bind the key values
+    let keys = KeyPress(String::from("Ctrl+"), 'N');
+    println!("\nKeys pressed: {}{}", keys.0, keys.1);
+        
+    // Instantiate WebEvent enum variants
+    // Set the boolean page Load value to true
+    let we_load = WebEvent::WELoad(true);
+    // Set the WEClick variant to use the data in the click struct
+    let we_click = WebEvent::WEClick(click);
+    // Set the WEKeys variant to use the data in the keys tuple
+    let we_key = WebEvent::WEKeys(keys);
+        
+    // Print the values in the WebEvent enum variants
+    // Use the {:#?} syntax to display the enum structure and data in a readable form
+    println!("\nWebEvent enum structure: \n\n {:?} \n\n {:?} \n\n {:#?}", we_load, we_click, we_key);
 }
